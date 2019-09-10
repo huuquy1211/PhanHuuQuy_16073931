9d6cabc9e209f57e85ec597b1fc1fbd50bc0d766
unknown <huuquy1211@gmail.com>
2019-09-11 00:03:29 +0700

    test

---
+++ app/src/main/java/com/example/phq_cau1/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.phq_cau1;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    CheckBox check_box;
+    Button dang_nhap, btnThoat;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        check_box = (CheckBox)findViewById(R.id.checkBox);
+        dang_nhap = (Button)findViewById(R.id.dang_nhap);
+        btnThoat = (Button)findViewById(R.id.btnThoat);
+
+        dang_nhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(check_box.isChecked()== true)
+                {
+                    Toast.makeText(MainActivity.this, "Chào mừng bạn đăng nhập hệ thống, thông tin của bạn đã được lưu", Toast.LENGTH_SHORT).show();
+
+                }
+                else
+                    Toast.makeText(MainActivity.this, "Chào mừng bạn đăng nhập hệ thống, thông tin của bạn không được lưu", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        final AlertDialog.Builder mydialog = new AlertDialog.Builder(this);
+        mydialog.setTitle("Thông Báo");
+        mydialog.setMessage("Bạn có muốn thoát không");
+
+        mydialog.setPositiveButton("Có", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        mydialog.setNegativeButton("Không", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+            }
+        });
+
+        btnThoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                AlertDialog alertDialog= mydialog.create();
+                alertDialog.show();
+            }
+        });
+    }
+}
